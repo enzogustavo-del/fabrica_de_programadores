f541201e48682d5f1e1185f57a98eeb6dfd413b7
enzogustavo-del <enzogustavo1334@gmail.com>
2026-04-30 16:57:55 -0300

    Add files via upload

---
+++ Aula8.py
@@ -0,0 +1,34 @@
+
+lista = ["Vanessa", "Carla", "Enzo", "Gustavo"]
+
+nome = input("Digite qualquer nome...\n\n")
+
+lista.append(nome)
+nome = input("Digite qualquer nome...\n\n")
+
+lista.append(nome)
+nome = input("Digite qualquer nome...\n\n")
+
+lista.append(nome)
+nome = input("Digite qualquer nome...\n\n")
+
+lista.append(nome)
+
+print(lista)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
