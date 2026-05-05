ad9418222281643a326dc35cb60170bc16157c5b
enzogustavo-del <enzogustavo1334@gmail.com>
2026-05-05 16:05:13 -0300

    Add files via upload

---
+++ aula9.py
@@ -0,0 +1,17 @@
+pessoa = {"nome":"Vanessa",
+          "idade":32,
+          "cor_cabelo":"marrom"
+          }
+
+pessoa["cor_cabelo"] = "loirinha"
+
+pessoa["nome"] = "Carla"
+
+pessoa["idade"] = 40
+
+print("\n", pessoa["nome"],"\n","\n", pessoa["cor_cabelo"],"\n",pessoa["idade"])
+
+
+
+
+
